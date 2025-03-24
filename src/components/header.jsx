11d9bdba4dd1e43f8f551b9f216f39b8136ca724
Devnatavaz <contato.natavaz@gmail.com>
2025-03-24 17:18:19 -0300

    atualização

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,11 +6,11 @@ export default function Header() {
   return ( 
     
 <div className=" m-5 ">
-  <img src="/Vitorino.jpg" className="w-40 h-40 rounded-full"/>
-  <ul className="flex gap-25  justify-end p-4 m-3 font-semibold ">
+  <img src="/Vitorino.jpg" className="w-20 h-20 rounded-full"/>
+  <ul className="flex gap-3  justify-end p-4 m-3 font-semibold ">
     <li><img src='/credit-card.svg' className="w-8 h-8 border-1 p-1.5 border-red-700 rounded-md"/></li>
     <li><img src='/store.svg' className="w-8 h-8 border-1 p-1.5 border-red-700 rounded-md"/></li>
-    <li><img src='/share.svg' className="w-8 h-8 border-1 p-1.5 border-red-700 rounded-md stroke-red-600 dark:stroke-pink-500 "/></li>
+    <li><img src='/share.svg' className="w-8 h-8 border-1 p-1.5 border-red-700 rounded-md stroke-red-600"/></li>
   </ul>
 </div>
 
